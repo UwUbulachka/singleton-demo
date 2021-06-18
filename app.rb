cd34463b09effcf1c3a435a6f09af564b8d7f831
UwUbulachka <meuw25@gmail.com>
2021-06-18 15:40:23 +0500

    update logger

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 class Logger
-  def say_something
+  def initialize
+   f = File.open 'log.txt', 'a'
+  end
+
+  def self.say_something
     puts "hello"
   end
+  
+  def log_something waht  
+    f.puts waht
+  end
 end  
+
 Logger.say_something
+
+logger = Logger.new
+Logger.log_something 'hello'
